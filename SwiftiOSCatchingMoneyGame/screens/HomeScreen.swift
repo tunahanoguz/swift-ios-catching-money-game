9013d2d78a685f81e808fe5e27b9c4e3fa7aba4e
tunahanoguz <tunahanog@gmail.com>
2020-08-12 12:20:11 +0300

    Create home screen

---
+++ SwiftiOSCatchingMoneyGame/screens/HomeScreen.swift
@@ -0,0 +1,34 @@
+//
+//  HomeScreen.swift
+//  SwiftiOSCatchingMoneyGame
+//
+//  Created by Tunahan Oğuz on 12.08.2020.
+//  Copyright © 2020 Tunahan Oğuz. All rights reserved.
+//
+
+import SwiftUI
+
+struct HomeScreen: View {
+    func printSomething() {
+        print("Print something.")
+    }
+    
+    var body: some View {
+        VStack(spacing: 30.0) {
+            HomeButton(text: "New game", buttonFunc: printSomething, firstColor: "#667eea", secondColor: "#764ba2", icon: "circle")
+            
+            HomeButton(text: "Show scores", buttonFunc: printSomething, firstColor: "#2af598", secondColor: "#009efd", icon: "waveform.path.ecg")
+            
+            HomeButton(text: "Show ratings", buttonFunc: printSomething, firstColor: "#6a11cb", secondColor: "#2575fc", icon: "rosette")
+            
+            HomeButton(text: "Sign out", buttonFunc: printSomething, firstColor: "#ff0844", secondColor: "#ffb199", icon: "arrow.right.to.line.alt")
+        }
+        .padding(30.0)
+    }
+}
+
+struct HomeScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeScreen()
+    }
+}
