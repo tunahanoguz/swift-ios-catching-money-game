d04049c170f636de2022fa60bc83fbf9a242438d
tunahanoguz <tunahanog@gmail.com>
2020-08-12 15:57:07 +0300

    Create scores screen and add TopTabBar component

---
+++ SwiftiOSCatchingMoneyGame/screens/ScoresScreen.swift
@@ -0,0 +1,21 @@
+//
+//  ScoresScreen.swift
+//  SwiftiOSCatchingMoneyGame
+//
+//  Created by Tunahan Oğuz on 12.08.2020.
+//  Copyright © 2020 Tunahan Oğuz. All rights reserved.
+//
+
+import SwiftUI
+
+struct ScoresScreen: View {
+    var body: some View {
+        TopTabBar()
+    }
+}
+
+struct ScoresScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        ScoresScreen()
+    }
+}
