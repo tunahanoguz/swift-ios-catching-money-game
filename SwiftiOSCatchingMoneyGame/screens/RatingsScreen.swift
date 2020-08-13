116af7edd367facaf328b2594ca1a500ba95c949
tunahanoguz <tunahanog@gmail.com>
2020-08-13 08:35:48 +0300

    Create ratings screen

---
+++ SwiftiOSCatchingMoneyGame/screens/RatingsScreen.swift
@@ -0,0 +1,21 @@
+//
+//  RatingsScreen.swift
+//  SwiftiOSCatchingMoneyGame
+//
+//  Created by Tunahan Oğuz on 13.08.2020.
+//  Copyright © 2020 Tunahan Oğuz. All rights reserved.
+//
+
+import SwiftUI
+
+struct RatingsScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RatingsScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        RatingsScreen()
+    }
+}
