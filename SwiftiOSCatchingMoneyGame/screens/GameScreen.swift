c257de3f3be6c1854edbd1c4dbc09dd4cfe8867b
tunahanoguz <tunahanog@gmail.com>
2020-08-13 10:58:01 +0300

    Create game screen

---
+++ SwiftiOSCatchingMoneyGame/screens/GameScreen.swift
@@ -0,0 +1,33 @@
+//
+//  GameScreen.swift
+//  SwiftiOSCatchingMoneyGame
+//
+//  Created by Tunahan Oğuz on 13.08.2020.
+//  Copyright © 2020 Tunahan Oğuz. All rights reserved.
+//
+
+import SwiftUI
+
+struct GameScreen: View {
+    var body: some View {
+        NavigationView {
+            VStack() {
+                Text("Time: 10")
+                    .font(.system(size: 20.0))
+                    .fontWeight(Font.Weight.medium)
+                
+                Text("Score: 0")
+                
+                Spacer()
+            }
+            .navigationBarHidden(true)
+            .padding(30.0)
+        }
+    }
+}
+
+struct GameScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        GameScreen()
+    }
+}
