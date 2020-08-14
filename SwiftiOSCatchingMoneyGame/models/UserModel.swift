338394c6c21e8b81884564a00fef88fbf0b31b62
tunahanoguz <tunahanog@gmail.com>
2020-08-14 09:44:26 +0300

    Create user model

---
+++ SwiftiOSCatchingMoneyGame/models/UserModel.swift
@@ -0,0 +1,17 @@
+//
+//  UserModel.swift
+//  SwiftiOSCatchingMoneyGame
+//
+//  Created by Tunahan Oğuz on 14.08.2020.
+//  Copyright © 2020 Tunahan Oğuz. All rights reserved.
+//
+
+import Foundation
+
+struct User {
+    var username: String
+    var email: String
+    var createdAt: Date
+    var gameType: Int
+    var gameLevel: Int
+}
