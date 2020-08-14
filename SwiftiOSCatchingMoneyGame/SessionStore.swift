fcbeeb79f1104fd78df6ac8465cad361acf20240
tunahanoguz <tunahanog@gmail.com>
2020-08-14 16:31:54 +0300

    Add userInfo variable to session store

--- SwiftiOSCatchingMoneyGame/SessionStore.swift
+++ SwiftiOSCatchingMoneyGame/SessionStore.swift
@@ -9,17 +9,20 @@
 import SwiftUI
 import Firebase
 import FirebaseAuth
+import FirebaseFirestore
 import Combine
 
 class SessionStore: ObservableObject {
     var didChange = PassthroughSubject<SessionStore, Never>()
     @Published var session: String? {didSet {self.didChange.send(self)}}
+    @Published var userInfo: UserModel? {didSet {self.didChange.send(self)}}
     var handle: AuthStateDidChangeListenerHandle?
     
     func listen() {
         handle = Auth.auth().addStateDidChangeListener({(auth, user) in
-            if let user = user {
-                self.session = user.uid
+            if user != nil {
+                self.session = user?.uid
+                self.getUserInfo(userID: user!.uid)
             } else {
                 self.session = nil
             }
@@ -29,6 +32,22 @@ class SessionStore: ObservableObject {
     func unbind() {
         if let handle = handle {
             Auth.auth().removeStateDidChangeListener(handle)
+        }
+    }
+    
+    func getUserInfo(userID: String) {
+        let firestore = Firestore.firestore()
+        let userCollection = firestore.collection("Users")
+        
+        userCollection.whereField("id", isEqualTo: userID)
+            .getDocuments { (querySnapshot, error) in
+                if let error = error {
+                    print(error)
+                } else {
+                    let userDoc = querySnapshot?.documents[0]
+                    let userData = userDoc?.data()
+                    self.userInfo = UserModel(id: userData?["id"] as! String, username: userData?["username"] as! String, email: userData?["email"] as! String, gameType: userData?["gameType"] as! Int, gameLevel: userData?["gameLevel"] as! Int)
+                }
         }
     }
     
