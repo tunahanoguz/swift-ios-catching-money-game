09ffd3695cfd9650cf6cb80cd671e0296f08e26b
tunahanoguz <tunahanog@gmail.com>
2020-08-15 11:55:53 +0300

    Add trim game type and game level functions to score detail screen

--- SwiftiOSCatchingMoneyGame/screens/ScoreDetailScreen.swift
+++ SwiftiOSCatchingMoneyGame/screens/ScoreDetailScreen.swift
@@ -9,13 +9,96 @@
 import SwiftUI
 
 struct ScoreDetailScreen: View {
+    @EnvironmentObject var topBarService: TopBarService
+    
+    var gameService: GameService = GameService()
+    var scoreID: String
+    
+    @State var game: GameModel = GameModel()
+    
+    func setGame(fetchedGame: GameModel) {
+        game = fetchedGame
+    }
+    
+    func trimGameType() -> String {
+        if game.gameType! == 0 {
+          return "Online"
+        } else {
+          return "Offline"
+        }
+    }
+    
+    func trimGameLevel() -> String {
+        let scoreLevel = game.gameLevel!
+        
+        if (scoreLevel == 0) {
+          return "Very Easy";
+        } else if (scoreLevel == 1) {
+          return "Easy";
+        } else if (scoreLevel == 2) {
+          return "Medium";
+        } else if (scoreLevel == 3) {
+          return "Hard";
+        } else {
+          return "Very Hard";
+        }
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            ZStack {
+                if (self.game.id != nil) {
+                    VStack {
+                        VStack {
+                            ScoreDetailItem(scoreTitle: "Game Date", scoreDescription: game.date!)
+                            
+                            ScoreDetailItem(scoreTitle: "Game Type", scoreDescription: trimGameType())
+                            
+                            ScoreDetailItem(scoreTitle: "Game Level", scoreDescription: trimGameLevel())
+                            
+                            ScoreDetailItem(scoreTitle: "Total Score", scoreDescription: String(game.scores!.score))
+                            
+                            ScoreDetailItem(scoreTitle: "TL Score", scoreDescription: String(game.scores!.tlScore))
+                            
+                            ScoreDetailItem(scoreTitle: "Dolar Score", scoreDescription: String(game.scores!.dolarScore))
+                        }
+                        
+                        VStack {
+                            ScoreDetailItem(scoreTitle: "Euro Score", scoreDescription: String(game.scores!.euroScore))
+                            
+                            ScoreDetailItem(scoreTitle: "Pound Score", scoreDescription: String(game.scores!.poundScore))
+                            
+                            ScoreDetailItem(scoreTitle: "Gold Score", scoreDescription: String(game.scores!.goldScore))
+                            
+                            ScoreDetailItem(scoreTitle: "Bitcoin Score", scoreDescription: String(game.scores!.bitcoinScore))
+                            
+                            ScoreDetailItem(scoreTitle: "Etherium Score", scoreDescription: String(game.scores!.etheriumScore))
+                            
+                            ScoreDetailItem(scoreTitle: "Dodge Score", scoreDescription: String(game.scores!.dodgeScore))
+                        }
+                    }
+                    .padding(.horizontal, 18.0)
+                } else {
+                    Text("Score details are loading...")
+                }
+                
+                Spacer()
+            }
+            .navigationBarTitle("Score Detail")
+            .navigationBarHidden(false)
+            .onAppear() {
+                self.gameService.getSingleGame(scoreID: self.scoreID, setGame: self.setGame)
+                
+//                self.topBarService.hideTabBar()
+//
+//                self.topBarService.hideNavigationBar()
+            }
+        }
     }
 }
 
 struct ScoreDetailScreen_Previews: PreviewProvider {
     static var previews: some View {
-        ScoreDetailScreen()
+        ScoreDetailScreen(scoreID: "aSis6aBxW3Kd8nCdYSzo")
     }
 }
