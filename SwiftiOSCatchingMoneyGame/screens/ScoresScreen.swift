46ed58954263f97b3970fdfb2c8d30cd8e4baba7
tunahanoguz <tunahanog@gmail.com>
2020-08-15 11:56:19 +0300

    Add top bar shown controlling to scores screen

--- SwiftiOSCatchingMoneyGame/screens/ScoresScreen.swift
+++ SwiftiOSCatchingMoneyGame/screens/ScoresScreen.swift
@@ -10,35 +10,42 @@ import SwiftUI
 import UIKit
 
 struct ScoresScreen: View {
+    @EnvironmentObject var topBarService: TopBarService
     @Environment(\.localStatusBarStyle) var statusBarStyle
+    
     @State var selectedIndex = 0
     
     var body: some View {
-        ZStack {
-            VStack {
-                TopTabBar(selectedIndex: $selectedIndex)
-                
-                GeometryReader {_ in
-                    VStack {
-                        if self.selectedIndex == 0 {
-                            OnlineScoresScreen()
-                        } else {
-                            OfflineScoresScreen()
+        NavigationView {
+            ZStack {
+                VStack {
+                    if self.topBarService.isShowed {
+                        TopTabBar(selectedIndex: $selectedIndex)
+                    }
+                    
+                    GeometryReader {_ in
+                        VStack {
+                            if self.selectedIndex == 0 {
+                                OnlineScoresScreen()
+                            } else {
+                                OfflineScoresScreen()
+                            }
                         }
                     }
                 }
+                .background(Color.white)
+                .onAppear {
+                    self.statusBarStyle.currentStyle = .lightContent
+                }
+                .onDisappear {
+                     self.statusBarStyle.currentStyle = .default
+                 }
             }
-            .background(Color.white)
-            .onAppear {
-                self.statusBarStyle.currentStyle = .lightContent
-            }
-            .onDisappear {
-                 self.statusBarStyle.currentStyle = .default
-             }
+            .padding(.top, UIApplication.shared.windows.last?.safeAreaInsets.top)
+            .background(Color.black)
+            .edgesIgnoringSafeArea(.all)
+            .onAppear(perform: self.topBarService.showTabBar)
         }
-        .padding(.top, UIApplication.shared.windows.last?.safeAreaInsets.top)
-        .background(Color.black)
-        .edgesIgnoringSafeArea(.all)
     }
 }
 
