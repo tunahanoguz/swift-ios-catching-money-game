40a3ff8c48d67dd1fd536b16d130c1426dd6ba02
tunahanoguz <tunahanog@gmail.com>
2020-08-15 08:25:04 +0300

    Create score detail screen

---
+++ SwiftiOSCatchingMoneyGame/screens/ScoreDetailScreen.swift
@@ -0,0 +1,21 @@
+//
+//  ScoreDetailScreen.swift
+//  SwiftiOSCatchingMoneyGame
+//
+//  Created by Tunahan Oğuz on 15.08.2020.
+//  Copyright © 2020 Tunahan Oğuz. All rights reserved.
+//
+
+import SwiftUI
+
+struct ScoreDetailScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ScoreDetailScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        ScoreDetailScreen()
+    }
+}
