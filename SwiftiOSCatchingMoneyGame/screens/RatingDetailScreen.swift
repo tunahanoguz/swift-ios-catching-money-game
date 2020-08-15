8cdaf249bf2be3a289efcc1ae8906f336d44ea6d
tunahanoguz <tunahanog@gmail.com>
2020-08-15 16:13:53 +0300

    Add score detail items in rating detail screen

--- SwiftiOSCatchingMoneyGame/screens/RatingDetailScreen.swift
+++ SwiftiOSCatchingMoneyGame/screens/RatingDetailScreen.swift
@@ -9,10 +9,69 @@
 import SwiftUI
 
 struct RatingDetailScreen: View {
+    var gameService: GameService = GameService()
+    var scoreID: String
+    
+    @State var game: GameModel = GameModel()
+    
+    func setGame(fetchedGame: GameModel) {
+        game = fetchedGame
+    }
+    
+    func trimGameType() -> String {
+        if game.gameType! == 0 {
+          return "Online"
+        } else {
+          return "Offline"
+        }
+    }
+    
+    func trimGameLevel() -> String {
+        let scoreLevel = game.gameLevel!
+        
+        if (scoreLevel == 0) {
+          return "Very Easy";
+        } else if (scoreLevel == 1) {
+          return "Easy";
+        } else if (scoreLevel == 2) {
+          return "Medium";
+        } else if (scoreLevel == 3) {
+          return "Hard";
+        } else {
+          return "Very Hard";
+        }
+    }
+    
     var body: some View {
         NavigationView {
             VStack {
-                Text("asd")
+                VStack {
+                    ScoreDetailItem(scoreTitle: "Game Date", scoreDescription: game.date!)
+                    
+                    ScoreDetailItem(scoreTitle: "Game Type", scoreDescription: trimGameType())
+                    
+                    ScoreDetailItem(scoreTitle: "Game Level", scoreDescription: trimGameLevel())
+                    
+                    ScoreDetailItem(scoreTitle: "Total Score", scoreDescription: String(game.scores!.score))
+                    
+                    ScoreDetailItem(scoreTitle: "TL Score", scoreDescription: String(game.scores!.tlScore))
+                    
+                    ScoreDetailItem(scoreTitle: "Dolar Score", scoreDescription: String(game.scores!.dolarScore))
+                }
+                
+                VStack {
+                    ScoreDetailItem(scoreTitle: "Euro Score", scoreDescription: String(game.scores!.euroScore))
+                    
+                    ScoreDetailItem(scoreTitle: "Pound Score", scoreDescription: String(game.scores!.poundScore))
+                    
+                    ScoreDetailItem(scoreTitle: "Gold Score", scoreDescription: String(game.scores!.goldScore))
+                    
+                    ScoreDetailItem(scoreTitle: "Bitcoin Score", scoreDescription: String(game.scores!.bitcoinScore))
+                    
+                    ScoreDetailItem(scoreTitle: "Etherium Score", scoreDescription: String(game.scores!.etheriumScore))
+                    
+                    ScoreDetailItem(scoreTitle: "Dodge Score", scoreDescription: String(game.scores!.dodgeScore))
+                }
             }
             .navigationBarTitle("Rating Detail")
             .navigationBarHidden(false)
@@ -22,6 +81,6 @@ struct RatingDetailScreen: View {
 
 struct RatingDetailScreen_Previews: PreviewProvider {
     static var previews: some View {
-        RatingDetailScreen()
+        RatingDetailScreen(scoreID: "aSis6aBxW3Kd8nCdYSzo")
     }
 }
