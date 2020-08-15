09f837e7f5d1d1d77135cb3abd25d196a387c9ca
tunahanoguz <tunahanog@gmail.com>
2020-08-15 19:29:32 +0300

    Add design codes to rating item component

--- SwiftiOSCatchingMoneyGame/components/RatingItem.swift
+++ SwiftiOSCatchingMoneyGame/components/RatingItem.swift
@@ -9,13 +9,39 @@
 import SwiftUI
 
 struct RatingItem: View {
+    var scoreID: String
+    var scoreType: Int
+    var totalScore: Int
+    var date: String
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationLink(destination: RatingDetailScreen(scoreID: scoreID)) {
+            HStack {
+                VStack {
+                    Text("Total Score")
+                    .font(.system(size: 14.0))
+                    
+                    Text(String(totalScore))
+                    .font(.system(size: 22.0))
+                    .fontWeight(Font.Weight.semibold)
+                }
+                
+                Spacer()
+                
+                Text(date)
+                .font(.system(size: 14.0))
+            }
+        }
+        .padding(.vertical, 20.0)
+        .padding(.horizontal, 16.0)
+        .background(Color.red)
+        .cornerRadius(8.0)
+        .foregroundColor(Color.white)
     }
 }
 
 struct RatingItem_Previews: PreviewProvider {
     static var previews: some View {
-        RatingItem()
+        RatingItem(scoreID: "scoreID", scoreType: 0, totalScore: 10, date: "2020 August, 12")
     }
 }
