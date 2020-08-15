1f3f00cc724a1db70c8b5b89e983eab0a83d4b79
tunahanoguz <tunahanog@gmail.com>
2020-08-15 16:31:55 +0300

    Create rating item component

---
+++ SwiftiOSCatchingMoneyGame/components/RatingItem.swift
@@ -0,0 +1,21 @@
+//
+//  RatingItem.swift
+//  SwiftiOSCatchingMoneyGame
+//
+//  Created by Tunahan Oğuz on 15.08.2020.
+//  Copyright © 2020 Tunahan Oğuz. All rights reserved.
+//
+
+import SwiftUI
+
+struct RatingItem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RatingItem_Previews: PreviewProvider {
+    static var previews: some View {
+        RatingItem()
+    }
+}
