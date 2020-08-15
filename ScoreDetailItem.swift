eb8413e6242cac8530c96b79351a17d9b9980bb4
tunahanoguz <tunahanog@gmail.com>
2020-08-15 08:26:24 +0300

    Create score detail item component

---
+++ ScoreDetailItem.swift
@@ -0,0 +1,20 @@
+//
+//  ScoreDetailItem.swift
+//  
+//
+//  Created by Tunahan Oğuz on 15.08.2020.
+//
+
+import SwiftUI
+
+struct ScoreDetailItem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ScoreDetailItem_Previews: PreviewProvider {
+    static var previews: some View {
+        ScoreDetailItem()
+    }
+}
