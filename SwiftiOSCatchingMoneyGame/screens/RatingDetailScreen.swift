4bb1f20dbdfa644483bc91987164ac3808b947bc
tunahanoguz <tunahanog@gmail.com>
2020-08-15 16:00:36 +0300

    Create rating detail screen

---
+++ SwiftiOSCatchingMoneyGame/screens/RatingDetailScreen.swift
@@ -0,0 +1,27 @@
+//
+//  RatingDetailScreen.swift
+//  SwiftiOSCatchingMoneyGame
+//
+//  Created by Tunahan Oğuz on 15.08.2020.
+//  Copyright © 2020 Tunahan Oğuz. All rights reserved.
+//
+
+import SwiftUI
+
+struct RatingDetailScreen: View {
+    var body: some View {
+        NavigationView {
+            VStack {
+                Text("asd")
+            }
+            .navigationBarTitle("Rating Detail")
+            .navigationBarHidden(false)
+        }
+    }
+}
+
+struct RatingDetailScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        RatingDetailScreen()
+    }
+}
