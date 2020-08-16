08f28c9e82087b11b9fa0a56edde7037bc377133
tunahanoguz <tunahanog@gmail.com>
2020-08-16 09:47:47 +0300

    Get offline games in OfflineScoresScreen

--- SwiftiOSCatchingMoneyGame/screens/OfflineScoresScreen.swift
+++ SwiftiOSCatchingMoneyGame/screens/OfflineScoresScreen.swift
@@ -9,21 +9,31 @@
 import SwiftUI
 
 struct OfflineScoresScreen: View {
+    @EnvironmentObject var session: SessionStore
+    var gameService: GameService = GameService()
+    
+    @State var games: [GameModel] = []
+    
+    func setGames(innerGames: [GameModel]) {
+        games = innerGames
+    }
+    
     var body: some View {
         VStack(alignment: .leading, spacing: 10.0) {
-            ScoreItem(scoreID: "123asda", scoreType: 0, totalScore: 100, date: "2020 August, 12")
-            
-            ScoreItem(scoreID: "123asda", scoreType: 0, totalScore: 100, date: "2020 August, 12")
-            
-            ScoreItem(scoreID: "123asda", scoreType: 0, totalScore: 100, date: "2020 August, 12")
-            
-            ScoreItem(scoreID: "123asda", scoreType: 0, totalScore: 100, date: "2020 August, 12")
-            
-            ScoreItem(scoreID: "123asda", scoreType: 0, totalScore: 100, date: "2020 August, 12")
+            if games.count != 0 {
+                ForEach(games) {game in
+                    ScoreItem(scoreID: game.id!, scoreType: game.gameType!, totalScore: game.scores!.score, date: game.date!)
+                }
+            } else {
+                Text("Games are loading...")
+            }
             
             Spacer()
         }
         .padding(.horizontal, 30.0)
+        .onAppear() {
+            self.gameService.getOfflineGames(userID: self.session.userInfo?.id ?? "", setGames: self.setGames)
+        }
     }
 }
 
