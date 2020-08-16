d469fa3ceed0fe7f8b3ef123f50add70e5d4d1b5
tunahanoguz <tunahanog@gmail.com>
2020-08-16 08:06:34 +0300

    Add ScoreRealmModel to GameRealmModel

--- SwiftiOSCatchingMoneyGame/models/GameRealmModel.swift
+++ SwiftiOSCatchingMoneyGame/models/GameRealmModel.swift
@@ -12,7 +12,7 @@ import RealmSwift
 class GameRealmModel: Object {
     @objc dynamic var id: String = ""
     @objc dynamic var userID: String = ""
-//    @objc dynamic var scores = List<ScoreModel>()
+    @objc dynamic var scores = List<ScoreRealmModel>()
     @objc dynamic var gameType: Int = 0
     @objc dynamic var gameLevel: Int = 0
     @objc dynamic var date: String = ""
