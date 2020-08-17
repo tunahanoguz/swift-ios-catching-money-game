c7a57f88c50e5ec646110f22105b263a909c86c1
tunahanoguz <tunahanog@gmail.com>
2020-08-17 10:40:37 +0300

    Remove navigation view from score detail screen

--- SwiftiOSCatchingMoneyGame/screens/ScoreDetailScreen.swift
+++ SwiftiOSCatchingMoneyGame/screens/ScoreDetailScreen.swift
@@ -9,8 +9,6 @@
 import SwiftUI
 
 struct ScoreDetailScreen: View {
-    @EnvironmentObject var topBarService: TopBarService
-    
     var gameService: GameService = GameService()
     var scoreID: String
     var gameType: Int
@@ -46,53 +44,51 @@ struct ScoreDetailScreen: View {
     }
     
     var body: some View {
-        NavigationView {
-            ZStack {
-                if (self.game.id != nil) {
+        ZStack {
+            if (self.game.id != nil) {
+                VStack {
                     VStack {
-                        VStack {
-                            ScoreDetailItem(scoreTitle: "Game Date", scoreDescription: game.date!)
-                            
-                            ScoreDetailItem(scoreTitle: "Game Type", scoreDescription: trimGameType())
-                            
-                            ScoreDetailItem(scoreTitle: "Game Level", scoreDescription: trimGameLevel())
-                            
-                            ScoreDetailItem(scoreTitle: "Total Score", scoreDescription: String(game.scores!.score))
-                            
-                            ScoreDetailItem(scoreTitle: "TL Score", scoreDescription: String(game.scores!.tlScore))
-                            
-                            ScoreDetailItem(scoreTitle: "Dolar Score", scoreDescription: String(game.scores!.dolarScore))
-                        }
+                        ScoreDetailItem(scoreTitle: "Game Date", scoreDescription: game.date!)
                         
-                        VStack {
-                            ScoreDetailItem(scoreTitle: "Euro Score", scoreDescription: String(game.scores!.euroScore))
-                            
-                            ScoreDetailItem(scoreTitle: "Pound Score", scoreDescription: String(game.scores!.poundScore))
-                            
-                            ScoreDetailItem(scoreTitle: "Gold Score", scoreDescription: String(game.scores!.goldScore))
-                            
-                            ScoreDetailItem(scoreTitle: "Bitcoin Score", scoreDescription: String(game.scores!.bitcoinScore))
-                            
-                            ScoreDetailItem(scoreTitle: "Etherium Score", scoreDescription: String(game.scores!.etheriumScore))
-                            
-                            ScoreDetailItem(scoreTitle: "Dodge Score", scoreDescription: String(game.scores!.dodgeScore))
-                        }
+                        ScoreDetailItem(scoreTitle: "Game Type", scoreDescription: trimGameType())
+                        
+                        ScoreDetailItem(scoreTitle: "Game Level", scoreDescription: trimGameLevel())
+                        
+                        ScoreDetailItem(scoreTitle: "Total Score", scoreDescription: String(game.scores!.score))
+                        
+                        ScoreDetailItem(scoreTitle: "TL Score", scoreDescription: String(game.scores!.tlScore))
+                        
+                        ScoreDetailItem(scoreTitle: "Dolar Score", scoreDescription: String(game.scores!.dolarScore))
                     }
-                    .padding(.horizontal, 18.0)
-                } else {
-                    Text("Score details are loading...")
+                    
+                    VStack {
+                        ScoreDetailItem(scoreTitle: "Euro Score", scoreDescription: String(game.scores!.euroScore))
+                        
+                        ScoreDetailItem(scoreTitle: "Pound Score", scoreDescription: String(game.scores!.poundScore))
+                        
+                        ScoreDetailItem(scoreTitle: "Gold Score", scoreDescription: String(game.scores!.goldScore))
+                        
+                        ScoreDetailItem(scoreTitle: "Bitcoin Score", scoreDescription: String(game.scores!.bitcoinScore))
+                        
+                        ScoreDetailItem(scoreTitle: "Etherium Score", scoreDescription: String(game.scores!.etheriumScore))
+                        
+                        ScoreDetailItem(scoreTitle: "Dodge Score", scoreDescription: String(game.scores!.dodgeScore))
+                    }
                 }
-                
-                Spacer()
+                .padding(.horizontal, 18.0)
+            } else {
+                Text("Score details are loading...")
             }
-            .navigationBarTitle("Score Detail")
-            .navigationBarHidden(false)
-            .onAppear() {
-                if self.gameType == 0 {
-                    self.gameService.getSingleGame(scoreID: self.scoreID, setGame: self.setGame)
-                } else {
-                    self.gameService.getSingleOfflineGame(scoreID: self.scoreID, setGame: self.setGame)
-                }
+            
+            Spacer()
+        }
+        .navigationBarTitle("Score Detail")
+        .navigationBarHidden(false)
+        .onAppear() {
+            if self.gameType == 0 {
+                self.gameService.getSingleGame(scoreID: self.scoreID, setGame: self.setGame)
+            } else {
+                self.gameService.getSingleOfflineGame(scoreID: self.scoreID, setGame: self.setGame)
             }
         }
     }
