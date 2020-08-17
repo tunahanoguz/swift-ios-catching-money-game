50f7ddc4f7d7bb797c96d1b663f85a10ad0a2a05
tunahanoguz <tunahanog@gmail.com>
2020-08-17 10:41:06 +0300

    Remove top tab bar from scores screen and add two button

--- SwiftiOSCatchingMoneyGame/screens/ScoresScreen.swift
+++ SwiftiOSCatchingMoneyGame/screens/ScoresScreen.swift
@@ -10,41 +10,27 @@ import SwiftUI
 import UIKit
 
 struct ScoresScreen: View {
-    @EnvironmentObject var topBarService: TopBarService
-    @Environment(\.localStatusBarStyle) var statusBarStyle
-    
-    @State var selectedIndex = 0
+    @State var isNavigationBarHidden: Bool = true
     
     var body: some View {
         NavigationView {
-            ZStack {
-                VStack {
-                    if self.topBarService.isShowed {
-                        TopTabBar(selectedIndex: $selectedIndex)
-                    }
-                    
-                    GeometryReader {_ in
-                        VStack {
-                            if self.selectedIndex == 0 {
-                                OnlineScoresScreen()
-                            } else {
-                                OfflineScoresScreen()
-                            }
-                        }
-                    }
+            VStack {
+                NavigationLink(destination: OnlineScoresScreen(isNavigationBarHidden: $isNavigationBarHidden)) {
+                    ScoresButton(text: "Online Games")
                 }
-                .background(Color.white)
-                .onAppear {
-                    self.statusBarStyle.currentStyle = .lightContent
+                .padding(.bottom, 18.0)
+                
+                NavigationLink(destination: OfflineScoresScreen(isNavigationBarHidden: $isNavigationBarHidden)) {
+                    ScoresButton(text: "Offline Games", firstColor: "#6a11cb", secondColor: "#2575fc")
                 }
-                .onDisappear {
-                     self.statusBarStyle.currentStyle = .default
-                 }
             }
-            .padding(.top, UIApplication.shared.windows.last?.safeAreaInsets.top)
-            .background(Color.black)
-            .edgesIgnoringSafeArea(.all)
-            .onAppear(perform: self.topBarService.showTabBar)
+            .padding(30.0)
+            .background(Color.white)
+            .navigationBarTitle("")
+            .navigationBarHidden(isNavigationBarHidden)
+            .onAppear() {
+                self.isNavigationBarHidden = true
+            }
         }
     }
 }
